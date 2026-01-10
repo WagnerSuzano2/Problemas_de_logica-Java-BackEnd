f39dc13af1e7c0529a6f92242021e64ab6a6f6b9
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2026-01-09 21:55:51 -0300

    chore: initial commit

---
+++ curso/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int y =32;
+            double x = 10.23233;
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println("Bom dia");
+    }
+}
