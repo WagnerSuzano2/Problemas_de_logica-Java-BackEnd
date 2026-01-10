7b57666302fc8bbff1f022431d5f57bad9a62780
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2026-01-10 01:32:01 -0300

    feat: Metodos de concatenar  no Java

--- curso/src/Concatenar_variaveis.java
+++ curso/src/Concatenar_variaveis.java
@@ -3,7 +3,7 @@ public class Concatenar_variaveis {
         String nome = "Maria";
         int idade = 31;
         double renda = 4000.0;
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda);
         //double x = 10.23233;
         //System.out.println("Resultado = "+ x +" Metros");
         //System.out.printf("Resultado = %.2f Metros%n", x);
