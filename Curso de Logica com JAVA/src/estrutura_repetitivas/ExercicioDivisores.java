9ad81bc05570649158af327205427edf54507a8a
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2026-02-24 23:59:57 -0300

    feat: Resolvido exercicio Divisores 1

---
+++ Curso de Logica com JAVA/src/estrutura_repetitivas/ExercicioDivisores.java
@@ -0,0 +1,17 @@
+package estrutura_repetitivas;
+
+import java.util.Scanner;
+
+public class ExercicioDivisores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            if (x % i == 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
