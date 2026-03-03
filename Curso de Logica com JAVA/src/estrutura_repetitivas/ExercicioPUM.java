1fb1482b7008376fea12fcc5562ffc8d70993403
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2026-03-02 23:22:39 -0300

    feat: Resolvido exercicio PUM

--- Curso de Logica com JAVA/src/estrutura_repetitivas/ExercicioPUM.java
+++ Curso de Logica com JAVA/src/estrutura_repetitivas/ExercicioPUM.java
@@ -7,13 +7,13 @@ public class ExercicioPUM {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int soma = 0;
-        for (int i = 0; i < x; i++) { // Roda N vezes (exatamente as linhas pedidas)
-            for (int j = 1; j <= 4; j++) { // Roda 4 vezes para cada linha
+        for (int i = 0; i < x; i++) {
+            for (int j = 1; j <= 4; j++) {
                 soma++;
                 if (soma % 4 == 0) {
-                    System.out.println("PUM"); // O quarto elemento quebra a linha
+                    System.out.println("PUM");
                 } else {
-                    System.out.print(soma + " "); // Os outros três ficam na mesma linha
+                    System.out.print(soma + " ");
                 }
             }
         }
